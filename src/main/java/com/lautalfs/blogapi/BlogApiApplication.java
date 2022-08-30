39436264b6e1a1fe9bafb5e1a94639b93e752d35
LautaLFS <lautafuriasse16@gmail.com>
2022-08-30 13:22:03 -0300

    Initial commit

---
+++ src/main/java/com/lautalfs/blogapi/BlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.lautalfs.blogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApiApplication.class, args);
+    }
+
+}
